1d3df94be905fab44d9d908e3a737c8e80fc7d99
Xicheng Tang <hellomico0520@hotmail.com>
2023-08-17 14:37:06 -0400

    Add solution

--- 08.17/2297. Jump Game VIII.py
+++ 08.17/2297. Jump Game VIII.py
@@ -9,35 +9,70 @@ Return the minimum cost to jump to the index n - 1.
 from collections import defaultdict
 
 
+# class Solution:
+#     def minCost(self, nums: list[int], costs: list[int]) -> int:
+#
+#         n = len(nums)
+#
+#         dic = defaultdict(set)
+#
+#         for i in range(n - 1):
+#             dic[i].add(i + 1)
+#
+#             if nums[i + 1] >= nums[i]:
+#                 k = i + 1
+#                 while k < n - 1 and nums[k] >= nums[i]:
+#                     k += 1
+#                 if nums[k] < nums[i]:
+#                     dic[i].add(k)
+#
+#             else:
+#                 k = i + 1
+#                 while k < n - 1 and nums[k] < nums[i]:
+#                     k += 1
+#                 if nums[k] >= nums[i]:
+#                     dic[i].add(k)
+#
+#         dp = [0] * n
+#
+#         for i in range(n - 2, -1, -1):
+#             dp[i] = min(costs[step] + dp[step] for step in dic[i])
+#
+#         print(dic)
+#         print(dp)
+#         return dp[0]
+
+from collections import defaultdict
+
+
 class Solution:
     def minCost(self, nums: list[int], costs: list[int]) -> int:
-
         n = len(nums)
 
-        dic = defaultdict(set)
+        possible = defaultdict(set)
+        maxstack = []
+        minstack = []
 
-        for i in range(n - 1):
-            dic[i].add(i + 1)
+        for j in range(n):
 
-            if nums[i + 1] >= nums[i]:
-                k = i + 1
-                while k < n - 1 and nums[k] >= nums[i]:
-                    k += 1
-                if nums[k] < nums[i]:
-                    dic[i].add(k)
+            while maxstack and nums[j] >= nums[maxstack[-1]]:
+                possible[maxstack.pop()].add(j)
 
-            else:
-                k = i + 1
-                while k < n - 1 and nums[k] < nums[i]:
-                    k += 1
-                if nums[k] >= nums[i]:
-                    dic[i].add(k)
+            while minstack and nums[j] < nums[minstack[-1]]:
+                possible[minstack.pop()].add(j)
+
+            maxstack.append(j)
+            minstack.append(j)
 
         dp = [0] * n
 
         for i in range(n - 2, -1, -1):
-            dp[i] = min(costs[step] + dp[step] for step in dic[i])
+            dp[i] = min(costs[step] + dp[step] for step in possible[i])
 
-        print(dic)
-        print(dp)
         return dp[0]
+
+
+nums = [3, 2, 4, 4, 1]
+costs = [3, 7, 6, 4, 2]
+x = Solution()
+print(x.minCost(nums, costs))
